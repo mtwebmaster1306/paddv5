94dda2a4691a4dcbc839212f38da12af690389d6
mtwebmaster1306 <hola@mejorandotuweb.com>
2024-08-13 01:53:04 +0000

    v1.5c

--- ListClientes.php
+++ ListClientes.php
@@ -85,7 +85,7 @@ include 'componentes/sidebar.php';
 </div>
 
 <div class="modal fade" id="actualizarcliente" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Editar Cliente</h5>
